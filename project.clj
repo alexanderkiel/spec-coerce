158f17c722ab7d35e256791a60e72acf3404e388
Alexander Kiel <alexanderkiel@gmx.net>
2018-10-03 19:19:52 +0200

    Bump Next Dev Version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.akiel/spec-coerce "0.2"
+(defproject org.clojars.akiel/spec-coerce "0.3-SNAPSHOT"
   :description "Coercion for Clojure Spec"
   :url "http://git.life.uni-leipzig.local/clojure/spec-coerce"
 
